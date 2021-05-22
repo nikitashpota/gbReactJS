456d6113c85c6a02fa4930533252d51bfa499435
Nikita <nikitashpota@ya.ru>
2021-05-22 18:19:02 +0300

    del Q1

--- project_workplace_1/src/components/message/message.js
+++ project_workplace_1/src/components/message/message.js
@@ -3,19 +3,6 @@ import PropTypes from "prop-types"
 import classNames from 'classnames';
 import styles from "../../index.module.css"
 
-function OutputMessage(props) {
-    const { message } = this.props
-    const { author, value } = message
-    return <h4 className={styles.messager__author}>{author}</h4>;
-}
-  
-function InputMessage(props) {
-    const { message } = this.props
-    const { author, value } = message
-    return <h4 className={styles.messager__author} className={styles.messager__author_right}>{author}</h4>;
-}
-
-
 export class Message extends React.Component {
     static propTypes = {
         message: PropTypes.shape({
@@ -27,23 +14,10 @@ export class Message extends React.Component {
     render() {
         const { message } = this.props
         const { author, value } = message
-
-
-        if(author === "Bot"){
             return (
                 <div className={styles.messager__userfield}>
-                    <h4 className={classNames(styles.messager__author, styles.messager__author_right)} >{author}</h4>
+                    <h4 className={author === "Bot" ? classNames(styles.messager__author, styles.messager__author_right): styles.messager__author}>{author}</h4>
                     <h3 className={styles.messager__text}>{value}</h3>
                 </div>)
-        }
-        else{
-            return (
-                <div className={styles.messager__userfield}>
-                    <h4 className={styles.messager__author}>{author}</h4>
-                    <h3 className={styles.messager__text}>{value}</h3>
-                </div>)
-        
-        }
-
     }
 }
