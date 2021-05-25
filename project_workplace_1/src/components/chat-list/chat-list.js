5d261bf4dd2c273322c6439d52f4efafbc8aea8c
nikitashpota <nikitashpota@ya.ru>
2021-05-25 17:12:46 +0300

    HW update styles

--- project_workplace_1/src/components/chat-list/chat-list.js
+++ project_workplace_1/src/components/chat-list/chat-list.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Chat } from "../chat"
+import { Chat } from "./chat"
 import PropTypes from "prop-types"
 import styles from "./chat-list.module.css"
 import List from '@material-ui/core/List';
